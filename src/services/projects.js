401f66ce94c11668ab1ac7f30aca7e72ae40ff32
Bryan Balderrama <bryxn.alex77@gmail.com>
2023-05-17 18:36:08 -0600

    First commit

---
+++ src/services/projects.js
@@ -0,0 +1,24 @@
+const Project = require("../models/project");
+
+exports.getProjects = async () => {
+  let projects = await Project.find().lean().exec();
+  return projects;
+};
+
+exports.getProjectById = async (id) => {
+    let project = await Project.findById(id).lean().exec();
+    return project;
+}
+
+exports.createProjects = async (requestBody) => {
+  let project = new Project({
+    name: requestBody.name,
+    projectLink: requestBody.projectLink,
+    description: requestBody.description,
+    overview: requestBody.overview,
+    imageUrl: requestBody.imageUrl,
+    tools: requestBody.tools,
+  });
+
+  return await project.save;
+};
